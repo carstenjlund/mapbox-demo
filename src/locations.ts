82cfaa14815d00100f695cb07d0464c3b2936780
Carsten <clu@rts.dk>
2025-02-01 21:29:22 +0100

    added map and markers

---
+++ src/locations.ts
@@ -0,0 +1,30 @@
+const mapLocations = [
+    {
+      name: "Tivoli",
+      latitude: 55.673676,
+      longitude: 12.568337
+    },
+    {
+      name: "Nyhavn",
+      latitude: 55.68030,
+      longitude: 12.58750
+    },
+    {
+      name: "Den Lille Havfrue",
+      latitude: 55.69291,
+      longitude: 12.59942
+    },
+    {
+      name: "Operahuset",
+      latitude: 55.68150,
+      longitude: 12.60075
+    },
+    {
+      name: "Frederiksberg Have",
+      latitude: 55.67388,
+      longitude: 12.52153
+    }
+  ];
+  
+  export default mapLocations;
+  
